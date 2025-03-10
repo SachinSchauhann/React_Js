353d6410c92fe65dc18ce6de860c46922a49be7c
SachinSchauhann <sachins9598@gmail.com>
2025-03-10 18:59:12 +0530

    Bootstrap

---
+++ bootstrap/src/App.jsx
@@ -0,0 +1,27 @@
+import { Col, Container, Row } from "react-bootstrap"
+
+
+function App() {
+
+
+  return (
+    <>
+    <Container>
+      <Row>
+        <Col >
+        <h1>first Column</h1>
+        <h2>Name :</h2>
+        </Col>
+        <Col>
+        <h1>Secound Column</h1>
+        <h2>Sachin Singh Chauhan</h2>
+
+        </Col>
+      </Row>
+    </Container>
+      
+    </>
+  )
+}
+
+export default App

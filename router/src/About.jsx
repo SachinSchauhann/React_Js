f37e8145d211527de1499b60401d0fbc495ace8f
SachinSchauhann <sachins9598@gmail.com>
2025-03-24 19:15:50 +0530

    router hook

--- router/src/About.jsx
+++ router/src/About.jsx
@@ -1,7 +1,14 @@
+import { useNavigate } from "react-router-dom"
+
 function About(){
+    let navigator = useNavigate()
+    function service (){
+        navigator ('/servises')
+    }
     return(
         <>
         <h1>About Page</h1>
+         <button onClick={service}> Services page 2</button>
         </>
     )
 }
--- router/src/Services.jsx
+++ router/src/Services.jsx
@@ -1,7 +1,8 @@
+
 function Services(){
     return(
         <>
-        <h1>Services Page</h1>
+        <h1>Services Page 1</h1>
         </>
     )
 }
